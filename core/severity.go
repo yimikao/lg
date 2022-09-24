26f5b84fb7165ceefdc7b52c3220cab2f57a7499
damilola olayinka <holayinkajr@gmail.com>
2022-09-24 19:10:47 +0100

    define severity levels

---
+++ core/severity.go
@@ -0,0 +1,32 @@
+package core
+
+import "strings"
+
+//severity identifies the type of log: info, error etc.
+type Severity int32
+
+//these constants identify the log levels in order of increasing severity.
+const (
+	Info Severity = iota
+	Warn
+	Error
+	Fatal
+)
+
+//Strings holds all severity levels in string forms indexed by there value
+var Strings = []string{
+	Info:  "INFO",
+	Warn:  "WARN",
+	Error: "ERROR",
+	Fatal: "FATAL",
+}
+
+//ByString returns a severity level by it's string form
+func ByString(s string) (Severity, bool) {
+	for sev, str := range Strings {
+		if str == strings.ToUpper(s) {
+			return Severity(sev), true
+		}
+	}
+	return 0, false
+}
