b18323b80feeab3e9be49c1cc52ab5c38d52cd47
damilola olayinka <holayinkajr@gmail.com>
2022-12-07 21:18:07 +0100

    severityLevel setable with a flag
    Signed-off-by: damilola olayinka <holayinkajr@gmail.com>

--- core/severity/severity.go
+++ core/severity/severity.go
@@ -1,11 +1,11 @@
-package core
+package severity
 
 import "strings"
 
-//severity identifies the type of log: info, error etc.
+// Severity identifies the type of log: info, error etc.
 type Severity int32
 
-//these constants identify the log levels in order of increasing severity.
+// These constants identify the log levels in order of increasing severity.
 const (
 	Info Severity = iota
 	Warn
@@ -13,7 +13,7 @@ const (
 	Fatal
 )
 
-//Strings holds all severity levels in string forms indexed by there value
+// Strings holds all severity levels in string forms indexed by there value
 var Strings = []string{
 	Info:  "INFO",
 	Warn:  "WARN",
@@ -21,12 +21,12 @@ var Strings = []string{
 	Fatal: "FATAL",
 }
 
-//ByString returns a severity level by it's string form
+// ByString returns a severity level by it's string form
 func ByString(s string) (Severity, bool) {
 	for sev, str := range Strings {
 		if str == strings.ToUpper(s) {
 			return Severity(sev), true
 		}
 	}
-	return 0, false
+	return -1, false
 }
--- ghost.go
+++ ghost.go
@@ -1,1 +1,61 @@
 package ghost
+
+import (
+	"strconv"
+	"sync/atomic"
+
+	"github.com/yimikao/ghost/core/severity"
+)
+
+type logger struct {
+	stderrThreshold severityValue
+}
+
+var globalLogger logger
+
+// severity identifies the sort of log: info, warning etc. It also implements
+// flag.Value interface. The -stderrthreshold flag is of type severity and
+// should be modified only through the flag.Value interface.
+type severityValue struct {
+	severity.Severity
+}
+
+// get returns the value of wrapped Severity.
+func (s *severityValue) get() severity.Severity {
+	return severity.Severity(atomic.LoadInt32((*int32)(&s.Severity)))
+}
+
+//set sets the value of wrapped Severity.
+func (s *severityValue) set(value severity.Severity) {
+	atomic.StoreInt32((*int32)(&s.Severity), int32(value))
+}
+
+// String is part of the flag.Value interface.
+func (s *severityValue) String() string {
+	return strconv.FormatInt(int64(s.Severity), 10)
+}
+
+// Get is part of the flag.Getter interface
+func (s *severityValue) Get() severity.Severity {
+	return s.Severity
+}
+
+// Set is part of the flag.Value interface.
+// set severity threshold by value passed.
+func (s *severityValue) Set(value string) error {
+	var threshold severity.Severity
+	//check if passed value matches any defined severity.
+	// CASE "INFO" etc
+	if sev, ok := severity.ByString(value); ok {
+		threshold = sev
+	} else {
+		// CASE "10" etc
+		sev, err := strconv.ParseInt(value, 10, 32)
+		if err != nil {
+			return err
+		}
+		threshold = severity.Severity(sev)
+	}
+	globalLogger.stderrThreshold.set(threshold)
+	return nil
+}
